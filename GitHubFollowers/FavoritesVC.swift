a09c78437f935f157bf21c636cb30f4d8dc675ad
Brian Fabello <bfabello@Brians-MacBook-Pro.local>
2020-10-05 14:40:57 -0700

    initial commit

---
+++ GitHubFollowers/FavoritesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  GitHubFollowers
+//
+//  Created by Brian Fabello on 10/2/20.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
