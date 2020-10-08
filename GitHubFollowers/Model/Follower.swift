05208a82d9d629777e4ffe2d0a8af139fd091831
Brian Fabello <bfabello@Brians-MacBook-Pro.local>
2020-10-08 14:52:50 -0700

    initialize user and follower model

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,16 @@
+//
+//  Followers.swift
+//  GitHubFollowers
+//
+//  Created by Brian Fabello on 10/8/20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    
+    var login: String
+    var avatar_url: String
+    
+    
+}
---
+++ GitHubFollowers/Model/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  GitHubFollowers
+//
+//  Created by Brian Fabello on 10/8/20.
+//
+
+import Foundation
+
+struct User: Codable{
+    
+    var login: String
+    var avatar_url: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var public_repos: Int
+    var public_gists: Int
+    var html_url: String
+    var following: Int
+    var followers: Int
+    var create_at: String
+    
+}
