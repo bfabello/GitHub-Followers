bdfcd24a8e4ddfbb3343b677c9a4573f6d29d587
Brian <brianfabello@gmail.com>
2020-10-22 17:22:38 -0700

    GFUserInfoHeaderVC - added subviews, layout constraints, configured UI elements + created SFSymbols enum

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Brian Fabello on 10/22/20.
+//
+
+import Foundation
+
+enum SFSymbols{
+    static let location = "mappin.and.ellipse"
+}
