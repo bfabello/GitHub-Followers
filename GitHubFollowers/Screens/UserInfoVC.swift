bfe161c852265727809e5c35a022a43174abc8a9
Brian <brianfabello@gmail.com>
2020-10-22 22:06:15 -0700

    configure UI layout in UserInfoVC that displays data & adds childVC to container

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class UserInfoVC: UIViewController {
-
+    
+    let headerView = UIView()
     var username: String!
     
     override func viewDidLoad() {
@@ -17,22 +18,45 @@ class UserInfoVC: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
 
-        showLoadingView()
+        layoutUI()
+
         // use capture list [weak self] to fix potential memory leaks
         NetworkManager.shared.getUserInfo(for: username) { [weak self] (result) in
             // unwraping optional of self so we dont need optional values for each call of self
             // introduced in Swift 4.2
             guard let self = self else { return }
-            self.dismissLoadingView()
             switch result {
-                case .success(let username):
-                    print(username)
-                                    
+                case .success(let user):
+                    DispatchQueue.main.async {
+                        self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                    }
                 case .failure(let error):
                     self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
-        
         }
+    }
+    
+    func layoutUI(){
+        view.addSubview(headerView)
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    // add a childVC to a containerView
+    func add(childVC: UIViewController, to containerView: UIView){
+        // add ViewController
+        addChild(childVC)
+        // add childVC to containerView
+        containerView.addSubview(childVC.view)
+        // fill up whole containerView
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+        
     }
     
     @objc func dismissVC(){
