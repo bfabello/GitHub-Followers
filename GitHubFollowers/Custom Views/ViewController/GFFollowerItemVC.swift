1cc5c8f5939fe18906f08d6b623f1f54999d2038
Brian <brianfabello@gmail.com>
2020-10-27 17:11:35 -0700

    configured items for follower container

---
+++ GitHubFollowers/Custom Views/ViewController/GFFollowerItemVC.swift
@@ -0,0 +1,23 @@
+//
+//  GFFollowerItemVC.swift
+//  GitHubFollowers
+//
+//  Created by Brian Fabello on 10/27/20.
+//
+// Subclass of GFItemInfoVC to inherit all its capabilities
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems(){
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewOne.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
