534e1c6ca8ca7ef07386ee234b583f02a50b0d7a
Brian <brianfabello@gmail.com>
2020-10-27 18:20:41 -0700

    fixed GFButton bug by forgetting to call configure in init that caused constraint errors

--- GitHubFollowers/Custom Views/ViewController/GFItemInfoVC.swift
+++ GitHubFollowers/Custom Views/ViewController/GFItemInfoVC.swift
@@ -14,6 +14,7 @@ class GFItemInfoVC: UIViewController {
     let itemInfoViewOne = GFItemInfoView()
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
+    
     var user: User!
     
     init(user: User){
