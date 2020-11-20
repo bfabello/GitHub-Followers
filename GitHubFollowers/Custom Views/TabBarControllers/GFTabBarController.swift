08ffbffdf743012e5377baf389e92f5307017533
Brian <brianfabello@gmail.com>
2020-11-20 15:38:08 -0800

    refactored SceneDelegate and moved code to custom GFTabBarController

---
+++ GitHubFollowers/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  GFTabBarController.swift
+//  GitHubFollowers
+//
+//  Created by Brian Fabello on 11/20/20.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNavigationController(), createFavoritesNavigationController()]
+    }
+    
+    func createSearchNavigationController() -> UINavigationController{
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNavigationController() -> UINavigationController{
+        let favoritesVC = FavoritesVC()
+        favoritesVC.title = "Favorites"
+        favoritesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoritesVC)
+    }
+}
