1909895ae6c2a31a63f4a28f25a5dca83896299a
Brian <brianfabello@gmail.com>
2020-11-20 16:17:37 -0800

    fixed required init in FollowerListVC

--- GitHubFollowers/Screens/FollowerListVC.swift
+++ GitHubFollowers/Screens/FollowerListVC.swift
@@ -33,6 +33,10 @@ class FollowerListVC: UIViewController {
         title = userName
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
