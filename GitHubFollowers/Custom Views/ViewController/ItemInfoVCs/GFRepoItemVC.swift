d0e642f3bb04d6bc17954ceafd85762433a4734a
Brian <brianfabello@gmail.com>
2021-01-05 16:48:59 -0800

    refactored initalizers in GFRepoItemVC & GFFollowerItemVC to conform to new delegates

--- GitHubFollowers/Custom Views/ViewController/ItemInfoVCs/GFRepoItemVC.swift
+++ GitHubFollowers/Custom Views/ViewController/ItemInfoVCs/GFRepoItemVC.swift
@@ -17,6 +17,14 @@ class GFRepoItemVC: GFItemInfoVC {
     // delegates must always be weak to avoid retain cycle
     weak var delegate: GFRepoItemInfoVCDelegate!
     
+    init(user: User, delegate: GFRepoItemInfoVCDelegate){
+        super.init(user: user)
+        self.delegate = delegate
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         configureItems()
--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -54,15 +54,9 @@ class UserInfoVC: UIViewController {
     }
     
     func configureUIElements(with user: User){
-        let repoItemVC = GFRepoItemVC(user: user)
-        repoItemVC.delegate = self
-        
-        let followerItemVC = GFFollowerItemVC(user: user)
-        followerItemVC.delegate = self
-
         self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
-        self.add(childVC: repoItemVC, to: self.itemViewOne)
-        self.add(childVC: followerItemVC, to: self.itemViewTwo)
+        self.add(childVC: GFRepoItemVC(user: user, delegate: self), to: self.itemViewOne)
+        self.add(childVC: GFFollowerItemVC(user: user, delegate: self), to: self.itemViewTwo)
         self.dateLabel.text = "GitHub since \(user.created_at.convertToDisplayFormat())"
     }
     
